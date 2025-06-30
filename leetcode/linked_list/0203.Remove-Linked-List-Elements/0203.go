277deea37bbd302ec1eef9529868a38c3147a09f
majiayong <aceshighbeita@gmail.com>
2025-06-30 23:45:53 +0800

    fix: update LC203,206

--- leetcode/linked_list/0203.Remove-Linked-List-Elements/0203.go
+++ leetcode/linked_list/0203.Remove-Linked-List-Elements/0203.go
@@ -2,6 +2,10 @@ package lianbiao
 
 import "algorithm/leetcode/structures"
 
+//
+// 力扣：https://leetcode.cn/problems/remove-linked-list-elements
+// 随想录：https://programmercarl.com/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -18,15 +22,16 @@ func removeElements(head *ListNode, val int) *ListNode {
 		return nil
 	}
 
-	dummyHead := &ListNode{}
-	dummyHead.Next = head
-	cur := dummyHead
-	for cur.Next != nil {
-		if cur.Next.Val == val {
-			cur.Next = cur.Next.Next
+	newHead := &ListNode{Val: 0, Next: head}
+	pre := newHead
+	cur := head
+	for cur != nil {
+		if cur.Val == val {
+			pre.Next = cur.Next
 		} else {
-			cur = cur.Next
+			pre = cur
 		}
+		cur = cur.Next
 	}
-	return dummyHead.Next
+	return newHead.Next
 }
--- leetcode/linked_list/0206.Reverse-Linked-List/0206.go
+++ leetcode/linked_list/0206.Reverse-Linked-List/0206.go
@@ -2,34 +2,64 @@ package lianbiao
 
 import "algorithm/leetcode/structures"
 
+// 反转链表
+// 力扣：https://leetcode.cn/problems/reverse-linked-list/description/
+// 随想录：https://programmercarl.com/0206.%E7%BF%BB%E8%BD%AC%E9%93%BE%E8%A1%A8.html#%E6%80%9D%E8%B7%AF
+
 type ListNode = structures.ListNode
 
 // 双指针
-// O(N) , O(1)
+// 时间复杂度：O(N)
+// 空间复杂度：O(1)
 func reverseList(head *ListNode) *ListNode {
-	var pre *ListNode
+	var prev *ListNode
 	cur := head
 	for cur != nil {
 		temp := cur.Next
-		cur.Next = pre
-		pre = cur
+		cur.Next = prev
+		prev = cur
 		cur = temp
 	}
-	return pre
+	return prev
 }
 
-// 递归
-// O(N), O(N)
+// TODO递归
+// 时间复杂度：O(N)
+// 空间复杂度：O(N)
 func reverseList2(head *ListNode) *ListNode {
-	return recur(nil, head)
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseList2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
 }
 
-func recur(pre, head *ListNode) *ListNode {
-	cur := head
-	if cur == nil {
-		return pre
-	}
-	temp := cur.Next
-	cur.Next = pre
-	return recur(cur, temp)
-}
+/*第1步：递归到底
+reverseList(1) 调用 reverseList(2)
+reverseList(2) 调用 reverseList(3)
+reverseList(3) 调用 reverseList(4)
+reverseList(4) 返回 4（因为 4.Next == nil）
+
+第2步：从后往前反转
+在 reverseList(3) 中：
+- newHead = 4
+- 3.Next.Next = 3  (让 4 指向 3)
+- 3.Next = nil
+- 返回 newHead (4)
+现在：4 → 3 → null
+
+在 reverseList(2) 中：
+- newHead = 4
+- 2.Next.Next = 2  (让 3 指向 2)
+- 2.Next = nil
+- 返回 newHead (4)
+现在：4 → 3 → 2 → null
+
+在 reverseList(1) 中：
+- newHead = 4
+- 1.Next.Next = 1  (让 2 指向 1)
+- 1.Next = nil
+- 返回 newHead (4)
+最终：4 → 3 → 2 → 1 → null*/
