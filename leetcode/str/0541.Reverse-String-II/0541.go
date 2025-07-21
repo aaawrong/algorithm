fc28b1a67206421aa4ee03a965fd87d122172ae1
majiayong <490550367@qq.com>
2025-07-22 00:29:45 +0800

    LC0541 反转字符串2

---
+++ leetcode/str/0541.Reverse-String-II/0541.go
@@ -0,0 +1,31 @@
+package str
+
+// 反转字符串II
+// 力扣：
+// https://leetcode.cn/problems/reverse-string-ii
+// 随想录：
+// https://programmercarl.com/0541.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2II.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+func reverseStr(s string, k int) string {
+	ss := []byte(s)
+	length := len(ss)
+	for i := 0; i < length; i += 2 * k {
+		if i+k <= length {
+			reverse(ss[i : i+k])
+		} else {
+			reverse(ss[i:length])
+		}
+	}
+	return string(ss)
+}
+
+func reverse(b []byte) {
+	left, right := 0, len(b)-1
+	for left < right {
+		b[left], b[right] = b[right], b[left]
+		left++
+		right--
+	}
+}
