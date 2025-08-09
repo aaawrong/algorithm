b4859b10ed9867f38278bbd96b2e12a7f7d8d716
majiayong <490550367@qq.com>
2025-08-10 00:33:57 +0800

    LC0232 用队列实现栈

---
+++ leetcode/stackAndQueue/0225.Implement-Stack-using-Queues/0225.go
@@ -0,0 +1,41 @@
+package stackAndQueue
+
+// 重复的子字符串
+// 力扣：
+// https://leetcode.cn/problems/implement-stack-using-queues
+// 随想录：
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+type MyStack struct {
+	// 单个队列实现，动图如下
+	// https://leetcode.cn/problems/implement-stack-using-queues/solutions/432204/yong-dui-lie-shi-xian-zhan-by-leetcode-solution/
+	queue []int
+}
+
+func Constructor() MyStack {
+	return MyStack{}
+}
+
+func (ms *MyStack) Push(x int) {
+	n := len(ms.queue)
+	ms.queue = append(ms.queue, x)
+	for ; n > 0; n-- {
+		ms.queue = append(ms.queue, ms.queue[0])
+		ms.queue = ms.queue[1:]
+	}
+}
+
+func (ms *MyStack) Pop() int {
+	x := ms.queue[0]
+	ms.queue = ms.queue[1:]
+	return x
+}
+
+func (ms *MyStack) Top() int {
+	return ms.queue[0]
+}
+
+func (ms *MyStack) Empty() bool {
+	return len(ms.queue) == 0
+}
---
+++ leetcode/stackAndQueue/0225.Implement-Stack-using-Queues/0225_two_queue.go
@@ -0,0 +1,42 @@
+package stackAndQueue
+
+// 重复的子字符串
+// 力扣：
+// https://leetcode.cn/problems/implement-stack-using-queues
+// 随想录：
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+type MyStack2 struct {
+	// 两个个队列实现，动图如下
+	// https://leetcode.cn/problems/implement-stack-using-queues/solutions/432204/yong-dui-lie-shi-xian-zhan-by-leetcode-solution/
+	// queue1负责存储，queue2负责进栈
+	queue1, queue2 []int
+}
+
+func Constructor2() MyStack2 {
+	return MyStack2{}
+}
+
+func (ms *MyStack2) Push(x int) {
+	ms.queue2 = append(ms.queue2, x)
+	for len(ms.queue1) > 0 {
+		ms.queue2 = append(ms.queue2, ms.queue1[0])
+		ms.queue1 = ms.queue1[1:]
+	}
+	ms.queue1, ms.queue2 = ms.queue2, ms.queue1
+}
+
+func (ms *MyStack2) Pop() int {
+	x := ms.queue1[0]
+	ms.queue1 = ms.queue1[1:]
+	return x
+}
+
+func (ms *MyStack2) Top() int {
+	return ms.queue1[0]
+}
+
+func (ms *MyStack2) Empty() bool {
+	return len(ms.queue1) == 0
+}
