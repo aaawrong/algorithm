9a974f5e9482988bec74202da1076b9f9331390e
majiayong <490550367@qq.com>
2025-11-18 23:36:43 +0800

    L0216 组合总和III

---
+++ leetcode/backtraking/0216.combinations-sum-III/0216.go
@@ -0,0 +1,19 @@
+package base_struct
+
+// 组合
+// 力扣：
+// https://leetcode.cn/problems/combinations/description/
+// 参考：
+
+// 时间复杂度：O(n * 2^n)， 遍历节点 O(2^n)，copy操作O(n)
+// 空间复杂度：O(n)
+
+func combine(n int, k int) [][]int {
+
+}
+
+// path表示路径已经存的元素
+func dfs(n, k, start int, path []int, res *[][]int) {
+
+	return
+}
