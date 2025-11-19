9678cf335b1ec19542c166810cc7db6bc13374e2
majiayong <490550367@qq.com>
2025-11-19 23:43:25 +0800

    L0216 组合总和III

--- leetcode/backtraking/0216.combinations-sum-III/0216.go
+++ leetcode/backtraking/0216.combinations-sum-III/0216.go
@@ -2,7 +2,7 @@ package base_struct
 
 // 组合
 // 力扣：
-// https://leetcode.cn/problems/combinations/description/
+// https://leetcode.cn/problems/combination-sum-iii/description/
 // 参考：
 
 // 时间复杂度：O(n * 2^n)， 遍历节点 O(2^n)，copy操作O(n)
