073fa224d303f7e98d261abf8d7b2205515544de
majiayong <490550367@qq.com>
2026-02-02 20:40:31 +0800

    feat: 0376 Wiggle-Subsequence

---
+++ leetcode/greedy/0376.Wiggle-Subsequence/0376.go
@@ -0,0 +1,32 @@
+package array
+
+// 摆动序列
+// 力扣：
+// https://leetcode.cn/problems/wiggle-subsequence/
+// 随想录：
+// https://programmercarl.com/0376.%E6%91%86%E5%8A%A8%E5%BA%8F%E5%88%97.html#%E6%80%9D%E8%B7%AF
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+
+func wiggleMaxLength(nums []int) int {
+	n := len(nums)
+	if n < 2 {
+		return n
+	}
+
+	res := 1
+	preDiff := nums[1] - nums[0]
+	if preDiff != 0 {
+		res = 2
+	}
+
+	for i := 2; i < n; i++ {
+		diff := nums[i] - nums[i-1]
+		if diff < 0 && preDiff >= 0 || diff > 0 && preDiff <= 0 {
+			res++
+			preDiff = diff
+		}
+	}
+	return res
+}
---
+++ leetcode/greedy/0376.Wiggle-Subsequence/0376_test.go
@@ -0,0 +1,77 @@
+package array
+
+import "testing"
+
+func TestWiggleMaxLength(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{
+			name: "empty array",
+			nums: []int{},
+			want: 0,
+		},
+		{
+			name: "single element",
+			nums: []int{5},
+			want: 1,
+		},
+		{
+			name: "two equal elements",
+			nums: []int{2, 2},
+			want: 1,
+		},
+		{
+			name: "two increasing elements",
+			nums: []int{1, 2},
+			want: 2,
+		},
+		{
+			name: "two decreasing elements",
+			nums: []int{2, 1},
+			want: 2,
+		},
+		{
+			name: "classic wiggle sequence",
+			nums: []int{1, 7, 4, 9, 2, 5},
+			want: 6,
+		},
+		{
+			name: "all equal",
+			nums: []int{3, 3, 3, 3},
+			want: 1,
+		},
+		{
+			name: "monotonic increasing",
+			nums: []int{1, 2, 3, 4, 5},
+			want: 2,
+		},
+		{
+			name: "monotonic decreasing",
+			nums: []int{5, 4, 3, 2, 1},
+			want: 2,
+		},
+		{
+			name: "with flat segments",
+			nums: []int{1, 1, 2, 2, 1},
+			want: 3,
+		},
+		{
+			name: "complex case",
+			nums: []int{1, 17, 5, 10, 13, 15, 10, 5, 16, 8},
+			want: 7,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := wiggleMaxLength(tt.nums)
+			if got != tt.want {
+				t.Fatalf("wiggleMaxLength(%v) = %d, want %d",
+					tt.nums, got, tt.want)
+			}
+		})
+	}
+}
