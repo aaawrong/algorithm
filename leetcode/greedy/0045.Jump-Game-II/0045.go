43b97131f7daae4dc691f377f2332b0d8a1882e4
majiayong <490550367@qq.com>
2026-02-09 23:19:27 +0800

    feat: 0045跳跃游戏II

---
+++ leetcode/greedy/0045.Jump-Game-II/0045.go
@@ -0,0 +1,29 @@
+package greedy
+
+// 跳跃游戏
+// 力扣：
+//
+// 随想录：
+//
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+func jump(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return 0
+	}
+
+	temp, cover, step := 0, 0, 0
+	for i := 0; i < n; i++ {
+		temp = max(temp, i+nums[i])
+		if i == cover {
+			step++
+			cover = temp
+			if cover >= n-1 {
+				break
+			}
+		}
+	}
+	return step
+}
