ed75dba3df6411aebe75027b0140112a43bce941
majiayong <490550367@qq.com>
2026-02-21 14:12:18 +0800

    feat: 0509 斐波那契数列

--- leetcode/dynamic_programming/0509.Fibonacci-Number/0509.go
+++ leetcode/dynamic_programming/0509.Fibonacci-Number/0509.go
@@ -2,8 +2,9 @@ package greedy
 
 // 斐波那契数列
 // 力扣：
-//
+// https://leetcode.cn/problems/fibonacci-number/
 // 随想录：
+// https://programmercarl.com/0509.%E6%96%90%E6%B3%A2%E9%82%A3%E5%A5%91%E6%95%B0.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
 
 // 时间复杂度：O(n)
 // 空间复杂度：O(1)
